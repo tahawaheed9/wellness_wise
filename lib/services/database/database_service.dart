277d68ff9675c8e6dc87522505cf8f00b7a7c276
tahawaheed9 <tahawaheed9@gmail.com>
2024-09-13 17:47:38 +0500

    Changes in AdditionalInformation, DatabaseServices and few others

--- lib/services/database/database_service.dart
+++ lib/services/database/database_service.dart
@@ -6,22 +6,14 @@ class DatabaseServices {
   final _firestore = FirebaseFirestore.instance;
   final _docId = AuthService.firebase().currentUser!.id;
 
-  late final bool _isDataSaved;
-
   late final DocumentReference _userDataDocumentRef;
   late final CollectionReference _predictionCollectionRef;
-  late final CollectionReference _additionalInformationCollectionRef;
 
   // Initializing the Document & Collection References...
   DatabaseServices() {
-    _isDataSaved = false;
-
     _userDataDocumentRef = _firestore.collection('user-data').doc(_docId);
 
     _predictionCollectionRef = _userDataDocumentRef.collection('predictions');
-
-    _additionalInformationCollectionRef =
-        _userDataDocumentRef.collection('additional-information');
   }
 
   // Setting Username and Gender to main collection (user-data)...
@@ -50,18 +42,5 @@ class DatabaseServices {
     } catch (error) {
       throw Exception(error.toString());
     }
-  }
-
-  // Adding new document to the 'additional-information' collection...
-  Future<bool> addAdditionalInformation(Map<String, Object?> data) async {
-    try {
-      await _additionalInformationCollectionRef
-          .add(data)
-          .whenComplete(<bool>() => true);
-    } catch (_) {
-      throw Exception(
-          'An error occurred. Please, Try again or Contact Support.');
-    }
-    return _isDataSaved;
   }
 }
